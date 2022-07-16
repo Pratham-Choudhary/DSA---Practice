f91e6ecc73667a7ac0dfea39641a4ca49a732c5f
Pratham Choudhary <104812995+Pratham-Choudhary@users.noreply.github.com>
2022-07-17 02:46:13 +0530

    Working Code

---
+++ GSoC-Assignments/Assignment-5/2_Keys_Keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int r=0;
+        for(int i=2;i<=n;i++)
+        {
+            while(n%i==0)
+            {
+                r+=i;
+                n=n/i;
+            }
+        }
+        return r;
+    }
+};
