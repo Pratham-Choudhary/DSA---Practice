fc08ac47dc18add5436c88e25827d108b5313100
Pratham Choudhary <104812995+Pratham-Choudhary@users.noreply.github.com>
2022-07-17 02:50:36 +0530

    Workinng Code

--- GSoC-Assignments/Assignment-5/Sum_Of_Prime_Factors.cpp
+++ GSoC-Assignments/Assignment-5/Sum_Of_Prime_Factors.cpp
@@ -1,84 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define int long long
 
-bool compare(string a,string b){
-    int i=0,j=0;
-    while(i<a.size() && a[i]=='0') 
-    {
-        i++;
-    }
-    while(j<b.size() && b[j]=='0')
-    {
-        j++;
-    }
-    if((a.size()-i)>(b.size()-j))
-    {
-        return 1;
-    }
-    if((a.size()-i)<(b.size()-j))
-    {
-        return 0;
-    }
-    
-    
-    while(i<a.size() && j<b.size() && a[i]==b[j])
-    {
-        i++;
-        j++;
-    }
-    if(i==a.size())
-    {
-        return 1;
-    }
-    return a[i]>b[j];
-}
 
-string toChar="0123456789";
-string substract(string a,string b)
-{
-    bool isNegative = false;
-    if(!compare(a,b)) {
-        swap(a,b);
-        isNegative = true;
-    }
-    string ans="";
-    int i=a.size()-1;
-    int j=b.size()-1;
-    while(i>=0 && j>=0)
-    {
-        if(a[i]>=b[j])
-        {
-          ans=ans+toChar[a[i]-b[j]];   
-        }
-        else
-        {
-            ans=ans+toChar[a[i]-b[j]+10];
-            int k=i-1;
-            while(k>=0 && a[k]=='0')
-            {
-                a[k]='9';
-                k--;
-            }
-            a[k]= toChar[a[k]-'1'];
-        }
-        i--;
-        j--;
-    }
-    
-    while(i>=0){
-        ans+=a[i];
-        i--;
-    }
-    if(isNegative){
-        ans+='-';
-    }
-    reverse(ans.begin(),ans.end());
+int getSumOfPrimeFactors(int n){
+    int ans=0;
+    //COMPLETE THE FUNCTION
     return ans;
-    
 }
 
-int main() {
-    if(compare("00011","00012")) cout<<"hey"<<"\n";
-    else cout<<"hello"<<"\n";
-    cout<<substract("100","1");
+
+signed main() {
+    
+    vector<int>A= {0, 2, 3, 4, 5, 5, 7, 6, 6, 7, 11, 7, 13, 9, 8, 8, 17, 8, 19, 9, 10, 13, 23, 9, 10, 15, 9, 11, 29, 10, 31, 10, 14, 19, 12, 10, 37, 21, 16, 11, 2734, 50010, 511, 111117, 285716, 6412, 90, 999985, 11352, 347, 1999969, 28585, 24703, 38478, 46554, 1559, 80009, 22744, 868, 52652, 1999979, 327, 27470, 1075, 2141, 2573, 1108, 333336, 1733, 21766, 984, 200006, 6133, 675, 1999993, 2080, 1042, 289, 105282, 79, 1745};
+    
+    Seive();
+    int test_pass=0;
+    for(int i=1;i<=40;i++){
+        if(getSumOfPrimeFactors(i)!=A[i-1]){
+            cout<<"Wrong Answer  for n:"<<i<<"\n";
+            break;
+        };
+        test_pass++;
+    }
+    
+    int x = 2e6-41;
+    for(int i=0;i<=40;i++){
+        if(getSumOfPrimeFactors(x+i)!=A[i+40]){
+            cout<<"Wrong Answer  for n:"<<x+i<<"\n";
+            break;
+        };
+        test_pass++;
+    }
+    cout<<"test_Passed: "<<test_pass<<"\n";
 }
